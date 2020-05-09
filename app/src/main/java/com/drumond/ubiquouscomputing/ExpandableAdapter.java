24ab611a510187b1ed76a214394c4c4180fb6238
Wanderley Drumond <wanderley.drumond@gmail.com>
2020-05-09 11:39:48 +0100

    First commit

---
+++ app/src/main/java/com/drumond/ubiquouscomputing/ExpandableAdapter.java
@@ -0,0 +1,110 @@
+package com.drumond.ubiquouscomputing;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ExpandableAdapter extends BaseExpandableListAdapter {
+
+    private List<String> listGroup;
+    private HashMap<String, List<String>> listData;
+    private LayoutInflater layoutInflater;
+
+    public ExpandableAdapter(Context context, List<String> listGroup, HashMap<String, List<String>> listData) {
+        this.listGroup = listGroup;
+        this.listData = listData;
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return listGroup.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return listData.get(listGroup.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return listGroup.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return listData.get(listGroup.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        ViewHolderGroup viewHolderGroup;
+
+        if (convertView == null) {
+            convertView = layoutInflater.inflate(R.layout.header_expandable_list_view, null);
+            viewHolderGroup = new ViewHolderGroup();
+            convertView.setTag(viewHolderGroup);
+
+            viewHolderGroup.tvGroup = (TextView) convertView.findViewById(R.id.tv_group);
+        } else {
+            viewHolderGroup = (ViewHolderGroup) convertView.getTag();
+        }
+
+        viewHolderGroup.tvGroup.setText(listGroup.get(groupPosition));
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        ViewHolderItem viewHolderItem;
+        String value = (String) getChild(groupPosition, childPosition);
+
+        if (convertView == null) {
+            convertView = layoutInflater.inflate(R.layout.item_expandable_list_view, null);
+            viewHolderItem = new ViewHolderItem();
+            convertView.setTag(viewHolderItem);
+
+            viewHolderItem.tvItem = convertView.findViewById(R.id.tv_item);
+        } else {
+            viewHolderItem = (ViewHolderItem) convertView.getTag();
+        }
+
+        viewHolderItem.tvItem.setText(value);
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+
+    class ViewHolderGroup {
+        TextView tvGroup;
+    }
+
+    class ViewHolderItem {
+        TextView tvItem;
+    }
+
+}
---
+++ app/src/main/java/com/drumond/ubiquouscomputing/MainActivity.java
@@ -0,0 +1,106 @@
+package com.drumond.ubiquouscomputing;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<String> listGroup;
+    private HashMap<String, List<String>> listData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buildList();
+
+        ExpandableListView expandableListView = findViewById(R.id.elv_expandableListView);
+        expandableListView.setAdapter(new ExpandableAdapter(MainActivity.this, listGroup, listData));
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                Toast.makeText(MainActivity.this, "Group: " + groupPosition + " | Item: " + childPosition, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(MainActivity.this, "Group: " + groupPosition + " opened", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(MainActivity.this, "Group: " + groupPosition + " closed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        expandableListView.setGroupIndicator(getResources().getDrawable(R.drawable.icon_group));
+        expandableListView.setGroupIndicator(ResourcesCompat.getDrawable(getResources(), R.drawable.icon_group, null));
+    }
+
+    public void buildList() {
+        listGroup = new ArrayList<>();
+        listData = new HashMap<>();
+
+        //    Resources
+        String location = getResources().getString(R.string.elv_h_location);
+        String accelerometer = getResources().getString(R.string.elv_h_accelerometer);
+        String deviceSensors = getResources().getString(R.string.elv_h_devicesensors);
+
+        String latitude = getResources().getString(R.string.elv_i_latitude);
+        String longitude = getResources().getString(R.string.elv_i_longitude);
+        String altitude = getResources().getString(R.string.elv_i_altitude);
+        String address = getResources().getString(R.string.elv_i_address);
+
+        String xAxis = getResources().getString(R.string.elv_i_xaxis);
+        String yAxis = getResources().getString(R.string.elv_i_yaxis);
+        String zAxis = getResources().getString(R.string.elv_i_zaxis);
+
+        String sensorsList = getResources().getString(R.string.elv_i_sensorslist);
+
+        //GROUP
+        listGroup.add(location);
+        listGroup.add(accelerometer);
+        listGroup.add(deviceSensors);
+        listGroup.add("Group 4");
+
+        //ITEM
+        List<String> auxiliaryList= new ArrayList<>();
+        auxiliaryList.add(latitude);
+        auxiliaryList.add(longitude);
+        auxiliaryList.add(altitude);
+        auxiliaryList.add(address);
+        listData.put(listGroup.get(0), auxiliaryList);
+
+        auxiliaryList= new ArrayList<>();
+        auxiliaryList.add(xAxis);
+        auxiliaryList.add(yAxis);
+        auxiliaryList.add(zAxis);
+        listData.put(listGroup.get(1), auxiliaryList);
+
+        auxiliaryList= new ArrayList<>();
+        auxiliaryList.add(sensorsList);
+        listData.put(listGroup.get(2), auxiliaryList);
+
+        auxiliaryList= new ArrayList<>();
+        auxiliaryList.add("Item 13");
+        auxiliaryList.add("Item 14");
+        auxiliaryList.add("Item 15");
+        auxiliaryList.add("Item 16");
+        listData.put(listGroup.get(3), auxiliaryList);
+    }
+}
